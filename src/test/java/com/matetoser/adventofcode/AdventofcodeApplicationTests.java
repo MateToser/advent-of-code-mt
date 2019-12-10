77333929ced5b3990955e1bf3b46cd424f05c4be
Máté TŐSÉR <mate.toser@shiwaforce.com>
2019-12-10 10:37:48 +0100

    Advent of Code Project

---
+++ src/test/java/com/matetoser/adventofcode/AdventofcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matetoser.adventofcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventofcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
